4b7cee54073349e611414a49ba062ab8fe4c7f53
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-04 21:07:15 +0100

    Update Regional.cpp

--- Regional.cpp
+++ Regional.cpp
@@ -4,21 +4,6 @@
 class Regional : public Train
 {
 public:
-<<<<<<< HEAD
-    void setSpeed(int current_speed)
-    {
-        speed = current_speed;
-    }
-    void setSpeedLimit()
-    {
-        speed = 80;
-        getTime();
-    }
-    void getPosition()
-    {
-    }
-};
-=======
     void setSpeed(int current_speed)    {   speed = current_speed;  }
     void setSpeedLimit()    {   speed = 80;    }
     void getPosition(){}
@@ -30,4 +15,3 @@ public:
         //setTimes?
     }
 };
->>>>>>> d106a117d184d6788caa60a1d7afc603428fea8a
