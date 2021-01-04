32d4608acc200d0a7db2d5004af4c0950d24832f
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-04 21:29:01 +0100

    Update Regional.cpp

--- Regional.cpp
+++ Regional.cpp
@@ -4,13 +4,16 @@
 class Regional : public Train
 {
 public:
-    void setSpeed(int current_speed)    {   speed = current_speed;  }
-    void setSpeedLimit()    {   speed = 80;    }
+    
+	void setSpeed(int current_speed)    {   speed = current_speed;  }
+    void setSpeedLimit()    {   speed = 80; }
     void getPosition(){}
+	void setName(int n) { name = n;   };
+	void setStart(int s) { start_time = s;   };
 
-    Regional(vector<int> vect){
-        setName(data[0]);
-        setStart(data[1]);
+	Regional(vector<int> vect){
+        setName(vect[0]);
+        setStart(vect[1]);
         setSpeed(160);
         //setTimes?
     }
