6ca31dede872229b636751c848c45f210342aabc
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-04 21:29:04 +0100

    Update SuperHighSpeed.cpp

--- SuperHighSpeed.cpp
+++ SuperHighSpeed.cpp
@@ -1,1 +1,17 @@
-#include "Train.h" 
+#include "Train.h"
+
+class Regional : public Train
+{
+public:
+    void setSpeed(int current_speed) { speed = current_speed; }
+    void setSpeedLimit() { speed = 80; }
+    void getPosition() {}
+
+    Regional(vector<int> vect)
+    {
+        setName(data[0]);
+        setStart(data[1]);
+        setSpeed(300);
+        //setTimes?
+    }
+};
