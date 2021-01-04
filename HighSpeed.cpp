0793771e5f964dc204e4c7e38967c904484cf2a4
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-04 23:58:34 +0100

    Update HighSpeed.cpp

--- HighSpeed.cpp
+++ HighSpeed.cpp
@@ -1,2 +1,19 @@
 #include "Train.h"
 
+class HishSpeed: public Train
+{
+public:
+	virtual void setSpeed(int current_speed) 
+	{
+		speed = current_speed;
+	}
+	virtual void setSpeedLimit() {}
+    virtual void getPosition() {}
+    virtual void setName(int n) {}
+	virtual void setStart(int s) {}
+	//virtual void getTime() = 0;
+	int speed;
+    int name;
+    int start_time;
+};
+
