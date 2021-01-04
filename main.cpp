c48b71c03089eed847f78ba84c17a2d91659805e
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-05 00:43:21 +0100

    Ho aggiunto l'acquisizione delle stazioni (probabilmente) abbastanza giusta

--- main.cpp
+++ main.cpp
@@ -3,18 +3,18 @@
 int main()
 {
     int number_of_trains = 0;
-    ifstream iput("timetables.txt");
-    while (!iput.eof())
+    ifstream input;
+    input.open("timetables.txt");
+    while (!input.eof())
     {
         string s;
-        getline(iput, s);
+        getline(input, s);
         number_of_trains++;
     }
-    iput.close();
-    ifstream input("timetables.txt");
+    input.close();
+    input.open("timetables.txt");
 
     vector<vector<int>> all_trains[number_of_trains];
-    int buffer = 0;
 
     while (input)
     {
@@ -29,8 +29,46 @@ int main()
             arr.push_back(num);
         }
         all_trains->push_back(arr); //associo ad un elemento dell'array di TRENI il vettore del singolo treno
-        buffer++;
     }
+    input.close();
+
+    int number_of_stations = 0;
+    input.open("line_description.txt");
+    while (!input.eof())
+    {
+        string s;
+        getline(input, s);
+        number_of_stations++;
+    }
+    input.close();
+    input.open("line_description.txt");
+
+    vector<vector<string>> all_stations[number_of_stations];
+
+    while (input)
+    {
+        vector<string> arr;
+        string input_line;
+        getline(input, input_line);
+        stringstream row;
+        row.str(input_line);
+        string num;
+        while (row >> num)
+        {
+            arr.push_back(num);
+        }
+        all_stations->push_back(arr); //associo ad un elemento dell'array di STAZIONI il vettore del singolo treno
+    }
+
+    // for (int i = 0; i < number_of_stations; i++)                                     PROVA DI STAMPA NON VA
+    // {
+    //     for (auto k = all_trains[i].begin(); k != all_stations[i].end(); k++)
+    //     {
+    //         cout << k << ' ';
+    //     }
+    // }
+
+    input.close();
 
     //for(int num : data)
     //    cout<<num<<endl;
