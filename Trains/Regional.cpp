34982bc497234f970b76842454a5ad965a2f6b58
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-06 19:46:36 +0100

    Update Regional.cpp

--- Trains/Regional.cpp
+++ Trains/Regional.cpp
@@ -1,25 +1,25 @@
-#include "Train.h"
+// #include "Train.h"
 
-class Regional : public Train
-{
-public:
-    void setSpeed(int current_speed) { speed = current_speed; }
-    void setSpeedLimit() { speed = 80; }
-    void getPosition() {}
-    void setName(int n) { name = n; };
-    void setStart(int s){};
-    void getTime();
+// class Regional : public Train
+// {
+// public:
+//     void setSpeed(int current_speed) { speed = current_speed; }
+//     void setSpeedLimit() { speed = 80; }
+//     void getPosition() {}
+//     void setName(int n) { name = n; };
+//     void setStart(int s){};
+//     void getTime();
 
-    Regional(vector<int> vect) //costruttore(?)
-    {
-        setName(vect[0]);
-        start_time = vect[1];
-        setSpeed(160);
-        //setTimes?
-    }
+//     Regional(vector<int> vect) //costruttore(?)
+//     {
+//         setName(vect[0]);
+//         start_time = vect[1];
+//         setSpeed(160);
+//         //setTimes?
+//     }
 
-    void checkTimesRegional(vector<int> orari)
-    {
-        //for (int i = 3; i<number_of_stations+3;i++)         //(come ci accedo??) a number of stations? l'ho eliminato perché efffettivamente basta guardare la dimensione dell'array di vettori
-    }
-};
+//     void checkTimesRegional(vector<int> orari)
+//     {
+//         //for (int i = 3; i<number_of_stations+3;i++)         //(come ci accedo??) a number of stations? l'ho eliminato perché efffettivamente basta guardare la dimensione dell'array di vettori
+//     }
+// };
