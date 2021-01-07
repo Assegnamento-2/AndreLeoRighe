3ebcfb9d8be60b1aa2d59638ff6d3d55cb6c1c10
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-07 23:01:54 +0100

    Update Train.cpp

--- Train.cpp
+++ Train.cpp
@@ -1,10 +1,17 @@
+#ifndef TRAIN1
+#define TRAIN1
 #include "T.h"
 
 class Train:public T{
 public:
-	Train(){
-		std::cout<<"Train.cpp constructor"<<std::endl;
-	}
+Train()
+{
+	std::cout << "Train.cpp constructor" << std::endl;
+}
+void Print()
+{
+	cout << "\nTRAIN";
+}
 };
 
-
+#endif
