4245c2684e151095fc7e10f8df39be89e26c187e
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-07 23:01:49 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+
 #include "Regional.cpp"
+#include "HighSpeed.cpp"
+//#include "HighSpeed.cpp"
 
 int main()
 {
-    Train* lll = new Regional();
+    Train s;
+    vector<Train> vec;
+    Regional k;
+    vec.push_back(k);
+    cout<<vec.size();
+    HighSpeed e;
+    s.Print();
+    k.Print();
+    e.Print();
+    //Train* lll = new Regional();
+    
     return 0;
 }
