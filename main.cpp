779434ef42c8b47dc287373ffafe082c0691897f
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-08 13:20:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@ int main()
     // int i = 0;
 
     // cout<<prova1.trenivect[1].max_speed;
-    vector<int> trenouno = prova1.all_trains[1];
-    cout << prova1.all_stations.size() << endl;
-    for (int i = 0; i < prova1.all_stations.size(); i++) //ALL TRAINS E ALL STATIONS OK! :)
-    {
-        for (auto k : prova1.all_stations[i])
-            cout << k << " ";
-        cout << endl;
-    }
+    // vector<int> trenouno = prova1.all_trains[1];
+    cout << prova1.all_stations.size() ;//<< endl;
+    // for (int i = 0; i < prova1.all_stations.size(); i++) //ALL TRAINS E ALL STATIONS OK! :)
+    // {
+    //     for (auto k : prova1.all_stations[i])
+    //         cout << k << " ";
+    //     cout << endl;
+    // }
 }
