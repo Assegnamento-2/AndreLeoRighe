64e69e3092afad218cbdc595ed5bbf6d9a25deb5
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-10 12:48:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,21 +9,27 @@ int main()
     // Train s;
     // Regional k;
 
-    vector<int> trenouno = prova1.all_trains[1];
+    //vector<int> trenouno = prova1.all_trains[1];
     //cout << prova1.all_stations.size() << endl;
     for (int i = 0; i < prova1.train_vect.size(); i++) //ALL TRAINS E ALL STATIONS OK! :)
     {
-        cout << prova1.train_vect[i].name << ' ';
+        if (prova1.train_vect[i].type == 1)
+            cout << "R ";
+        if (prova1.train_vect[i].type == 2)
+            cout << "V ";
+        else
+            cout << "S ";
+
+        cout << prova1.train_vect[i].name << ": ";
         for (int k = 0; k < prova1.train_vect[i].eta.size(); k++)
             cout << prova1.train_vect[i].eta[k] << ' ';
         cout << endl;
     }
-    // for (int i = 0; i < prova1.all_stations.size(); i++)
-    // {
-    //     for (int k = 0; k < prova1.all_stations[i].size(); k++)
-    //     {
-    //         cout << prova1.all_stations[i][k] << ' ';
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 0; i < prova1.station_vect.size(); i++)
+    {
+        cout << prova1.station_vect[i].name << ' ' << prova1.station_vect[i].distance;
+        if (prova1.station_vect[i].type == 0)
+            cout << " P";
+        cout << endl;
+    }
 }
