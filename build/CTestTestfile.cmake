007103179167f7034f803a83341dc798f2455675
Calciooo <76959048+Calciooo@users.noreply.github.com>
2021-01-12 19:03:10 +0100

    f

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/andre/Documents/GitHub/AndreLeoRighe
+# Build directory: C:/Users/andre/Documents/GitHub/AndreLeoRighe/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
