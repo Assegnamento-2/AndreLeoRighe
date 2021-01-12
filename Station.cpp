8f348a98b7b6cfec8a195d85232834b20d6e988d
leofagg <76787659+leofagg@users.noreply.github.com>
2021-01-12 18:43:11 +0100

    Update Station.cpp

--- Station.cpp
+++ Station.cpp
@@ -17,91 +17,90 @@ void Station::manage(vector<Train> &vect, const int time)
             if (i != 0 && vect[i - 1].current_pos < distance - 5 && vect[i - 1].current_pos - vect[i].current_pos <= 10) //se il treno precedente non è ancora arrivato al parcheggio e la distanza fra i due è <= 10km (e se non è il primo treno gestito, che per come è ordinato il vettore, non puô avere treni che lo rallentano)
             {
                 vect[i].setSpeed(vect[i - 1].current_speed); //setta la velocità del treno dietro uguale a quello che lo precede NON CREDO SERVA CONTROLLARE GLI ALTRI
-                                                             // NON CREDO SERVA CONTROLLARE GLI ALTRI
+                                                             // NON CREDO SERVA CONTROLLARE GLI ALTRI           }
             }
-        }
-        else if (vect[i].current_pos >= distance - 5 && vect[i].current_speed > 80.00 / 60.00 && !vect[i].is_parked) //quando arriva a 5km(e non è già nel parcheggio)
-        {
-            if (free_binaries > 0) //se ci sono binari liberi ne occupa uno e parte verso la stazione
+            else if (vect[i].current_pos >= distance - 5 && vect[i].current_speed > 80.00 / 60.00 && !vect[i].is_parked) //quando arriva a 5km(e non è già nel parcheggio)
             {
-                free_binaries--;
-                vect[i].setSpeedLimit();
-            }
-            else if (free_binaries == 0)
-            {
-                vect[i].wait = 0;
-                vect[i].is_parked = true;
-            }
-        }
-        else if (vect[i].is_parked && free_binaries > 0) //treno nel parcheggio e banchine disponibili il treno vi accede
-        {
-            vect[i].setSpeedLimit();
-            vect[i].is_parked = false;
-            vect[i].wait = 0;
-            free_binaries--;
-        }
-        else if (vect[i].is_parked && free_binaries == 0) //treno nel parcheggio e banchine piene il treno resta nel parcheggio
-        {
-            vect[i].wait++;
-        }
-        else if (vect[i].current_pos >= distance && vect[i].current_pos < distance +(80.00 / 60.00)-0.1)//- 1 + 2.00 / 3 + (80.00 / 60.00) + 1.00 / 3) //se il treno è  nella stazione (0.1 approssimazione della distanza)
-        {
-            if (vect[i].wait == 0)
-            {
-                vect[i].current_pos = distance;
-                cout << "Il treno " << vect[i].name << " è arrivato alla stazione " << name << " alle " << time;
-                if (time > vect[i].eta[station_number])
-                    cout << ", con un ritardo di " << time - vect[i].eta[station_number] << " minuti";
-                if (time == vect[i].eta[station_number])
-                    cout << ", perfettamente in orario!";
-                if (time < vect[i].eta[station_number])
-                    cout << ", in anticipo di " << vect[i].eta[station_number] - time << " minuti";
-                cout << endl;
-            }
-            if (vect[i].wait == 5) //se sono passati 5 minuti può partire   vect[i].current_pos == distance &&
-            {
-                vect[i].wait = 0;
-                vect[i].setSpeedLimit();
-                free_binaries++;
-                cout << "TRENO PARTITO DA " << name << " alle " << time << endl;
-                if (type == 2)
+                if (free_binaries > 0) //se ci sono binari liberi ne occupa uno e parte verso la stazione
                 {
-                    vect[i].has_arrived = true;
-                    vect[i].setSpeed(0);
+                    free_binaries--;
+                    vect[i].setSpeedLimit();
+                }
+                else if (free_binaries == 0)
+                {
+                    vect[i].wait = 0;
+                    vect[i].is_parked = true;
                 }
             }
-            else if (vect[i].wait < 5) //se sono passati meno di 5 minuti, continua ad aspettare
+            else if (vect[i].is_parked && free_binaries > 0) //treno nel parcheggio e banchine disponibili il treno vi accede
             {
-                vect[i].setSpeed(0);
+                vect[i].setSpeedLimit();
+                vect[i].is_parked = false;
+                vect[i].wait = 0;
+                free_binaries--;
+            }
+            else if (vect[i].is_parked && free_binaries == 0) //treno nel parcheggio e banchine piene il treno resta nel parcheggio
+            {
                 vect[i].wait++;
             }
-            // if (vect[i].current_pos == distance && vect[i].wait == 5) //se sono passati 5 minuti può partire
-            // {
-            //     vect[i].wait = 0;
-            //     vect[i].setSpeedLimit();
-            //     free_binaries++;
-            // }
+            else if (vect[i].current_pos >= distance && vect[i].current_pos < distance + (80.00 / 60.00) - 0.1) //- 1 + 2.00 / 3 + (80.00 / 60.00) + 1.00 / 3) //se il treno è  nella stazione (0.1 approssimazione della distanza)
+            {
+                if (vect[i].wait == 0)
+                {
+                    vect[i].current_pos = distance;
+                    cout << "Il treno " << vect[i].name << " è arrivato alla stazione " << name << " alle " << time;
+                    if (time > vect[i].eta[station_number])
+                        cout << ", con un ritardo di " << time - vect[i].eta[station_number] << " minuti";
+                    if (time == vect[i].eta[station_number])
+                        cout << ", perfettamente in orario!";
+                    if (time < vect[i].eta[station_number])
+                        cout << ", in anticipo di " << vect[i].eta[station_number] - time << " minuti";
+                    cout << endl;
+                }
+                if (vect[i].wait == 5) //se sono passati 5 minuti può partire   vect[i].current_pos == distance &&
+                {
+                    vect[i].wait = 0;
+                    vect[i].setSpeedLimit();
+                    free_binaries++;
+                    cout << "TRENO PARTITO DA " << name << " alle " << time << endl;
+                    if (type == 2)
+                    {
+                        vect[i].has_arrived = true;
+                        vect[i].setSpeed(0);
+                    }
+                }
+                else if (vect[i].wait < 5) //se sono passati meno di 5 minuti, continua ad aspettare
+                {
+                    vect[i].setSpeed(0);
+                    vect[i].wait++;
+                }
+                // if (vect[i].current_pos == distance && vect[i].wait == 5) //se sono passati 5 minuti può partire
+                // {
+                //     vect[i].wait = 0;
+                //     vect[i].setSpeedLimit();
+                //     free_binaries++;
+                // }
+            }
+            vect[i].updatePosition();
+            vect[i].moved = true;
         }
-        vect[i].updatePosition();
-        vect[i].moved = true;
     }
-}
-void Station::start(vector<Train> &vect, const int time)
-{
-
-    for (int i = 0; i < vect.size(); i++)
+    void Station::start(vector<Train> & vect, const int time)
     {
-        if (i != 0 && (vect[i].start_time - vect[i - 1].start_time) < 6)
+
+        for (int i = 0; i < vect.size(); i++)
         {
-            vect[i].start_time = 6 + vect[i - 1].start_time;
+            if (i != 0 && (vect[i].start_time - vect[i - 1].start_time) < 6) //se due treni hanno orari di partenza troppo vicini l'orario di partenza del secondo treno viene aumentato di 6(tempo necessario per dare modo al primo treno di prendere 10km di vantaggio)
+            {
+                vect[i].start_time = 6 + vect[i - 1].start_time;
+            }
+            else if (vect[i].start_time == time) //&& (vect[i].start_time - vect[i - 1].start_time) > 6)
+            {
+                vect[i].setSpeedLimit();
+                cout << "Il treno " << vect[i].name << " è partito dalla stazione " << name << " alle " << time << endl;
+            }
+            vect[i].updatePosition();
+            vect[i].moved = true;
         }
-        else if (vect[i].start_time == time) //&& (vect[i].start_time - vect[i - 1].start_time) > 6)
-        {
-            vect[i].setSpeedLimit();
-            cout << "Il treno " << vect[i].name << " è partito dalla stazione " << name << " alle " << time << endl;
-        }
-        vect[i].updatePosition();
-        vect[i].moved = true;
     }
-}
 #endif
